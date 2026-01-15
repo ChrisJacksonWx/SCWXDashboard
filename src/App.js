88669551f69c2b0c57f13319c4b944c81627ee18
Chris Jackson <chris@myscweather.com>
2026-01-15 10:19:03 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import WinterWeatherOpsDashboard from './WinterWeatherOpsDashboard';
+import './App.css';
+
+function App() {
+  return <WinterWeatherOpsDashboard />;
+}
+
+export default App;
